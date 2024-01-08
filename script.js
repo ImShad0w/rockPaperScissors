bbca4305e7b56f6bc6a744b1afaca0293a895796
ImShad0w <kirill19042006@gmail.com>
2024-01-08 18:53:20 +0100

    Added functions

---
+++ script.js
@@ -0,0 +1,29 @@
+//What do we need to do?
+
+//Create a random number generator between 1 and 3 
+//For each number assign a constant like rock papper or scissors
+//Then create an if loop that will get the number that we will input against the computerchoice
+//Create another if loop for comparing the values to see the winner e.g if our number or input is rock and computer generates paper it will have a point
+//Else we have a point
+
+
+//Random number generator between 1 and 3
+function randomNumber(){
+    let randomInt = Math.floor(Math.random()* 3);
+    console.log(randomInt);
+
+}
+//For each number assign a variable
+const rock = 0
+const scissors = 1
+const paper = 2
+
+
+
+//Create a math operation that will substract the input user value from the machine value
+
+
+
+
+
+
