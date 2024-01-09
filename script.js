adaba1a6e2c374c8a31797ba3b237ec8bd8ba098
ImShad0w <kirill19042006@gmail.com>
2024-01-09 13:07:51 +0100

    Created functions, polished code for easier understanding

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
 //What do we need to do?
 
-//Create a random number generator between 1 and 3 
-//For each number assign a constant like rock papper or scissors
-//Then create an if loop that will get the number that we will input against the computerchoice
-//Create another if loop for comparing the values to see the winner e.g if our number or input is rock and computer generates paper it will have a point
-//Else we have a point
+//Create an array of choices for the computer to choose from
 
+//Create a operation that will output the result of the play e.g if the computer chooses paper and we choose rock we lose
 
-//Random number generator between 1 and 3
-function randomNumber(){
-    let randomInt = Math.floor(Math.random()* 3);
-    console.log(randomInt);
+//Create a function that will strat the game by prompting us to get the result
 
+//Computer chooses a value from the array based on the arrays length and outputs the result
+function computerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
 }
-//For each number assign a variable
-const rock = 0
-const scissors = 1
-const paper = 2
 
+//Plays round
+function playRound(userChoice, compChoice){
+    //Prints the choices
+    console.log(`Your choice: ${userChoice}`)
+    console.log(`Computer choice: ${compChoice}`)
 
+    if (userChoice === compChoice){
+        return "Its a tie!";//If the choices are equal print tie
+    }
+        else if (userChoice === "rock" && compChoice ==="scissors"){//Rock vs scissors
+            return "You Win!";
+        }
+        else if (userChoice === "scissors" && compChoice === "rock"){//scissors vs rock
+            return "You loose!";
+        }
+        else if (userChoice === "paper" && compChoice ==="rock"){//paper vs rock
+            return "You Win!";
+        }
+        else if (userChoice === "rock" && compChoice === "paper"){//rock vs paper
+            return "You loose!";
+        }
+        else if (userChoice === "scissors" && compChoice ==="paper"){//scissors vs paper
+            return "You Win!";
+        }
+        else if (userChoice === "paper" && compChoice === "scissors"){//paper vs scissors
+            return "You loose!";
+        }
+    }
 
-//Create a math operation that will substract the input user value from the machine value
+//Starts game
 
+function startGame(){
+    const userChoice = prompt("Choose between rock, paper, scissors: ").toLowerCase();//Prompts user for input
+    const compChoice = computerChoice();//Assigns constant to the result of the function
+    const result = playRound(userChoice, compChoice); //Gets the result of the round
+    console.log(result); //Outputs the result
+}
 
-
-
-
-
+startGame();
