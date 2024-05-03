bc150a4e6c309a12dc005ae4e68cc4b8deb7dac6
ImShad0w <kirill19042006@gmail.com>
2024-05-04 00:22:39 +0200

    Polishing code, creating new things

--- script.js
+++ script.js
@@ -8,25 +8,6 @@
 
 //Computer chooses a value from the array based on the arrays length and outputs the result
 
-window.onload = function() {
-    const container = document.createElement("div");
-    container.id = "container";
-    const parent = document.querySelector("body");
-    parent.appendChild(container);
-    const butt1 = document.createElement("button");
-    butt1.textContent = "Rock";
-    const butt2 = document.createElement("button");
-    butt2.textContent = "Paper";
-    const butt3 = document.createElement("button");
-    butt3.textContent = "Scissors";
-    container.appendChild(butt1);
-    container.appendChild(butt2);
-    container.appendChild(butt3);
-    
-  butt1.addEventListener("click"){
-    userChoice = "rock";
-  }
-}
 
 
 
@@ -47,32 +28,32 @@ function playRound(userChoice, compChoice){
         startGame(); //Starts the game again
     }
         else if (userChoice === "rock" && compChoice ==="scissors"){//Rock vs scissors
-            return("You Win!");
+            console.log("You Win!");
         }
         else if (userChoice === "scissors" && compChoice === "rock"){//scissors vs rock
-           return("You loose!");
+            console.log("You loose!");
         }
         else if (userChoice === "paper" && compChoice ==="rock"){//paper vs rock
-           return("You Win!");
+            console.log("You Win!");
         }
         else if (userChoice === "rock" && compChoice === "paper"){//rock vs paper
-            return("You loose!");
+            console.log("You loose!");
         }
         else if (userChoice === "scissors" && compChoice ==="paper"){//scissors vs paper
-            return("You Win!");
+            console.log("You Win!");
         }
         else if (userChoice === "paper" && compChoice === "scissors"){//paper vs scissors
-            return("You loose!");
+            console.log("You loose!");
         }
     }
 
 //Starts game
 
 function startGame(){
-    let userChoice;//Prompts user for input
+    const userChoice = prompt("Write down you're choice:");//Prompts user for input
     const compChoice = computerChoice();//Assigns constant to the result of the function
     const result = playRound(userChoice, compChoice) //Gets the result of the round
-    console.log(result) //Outputs the result of the round
+     //Outputs the result of the round
 }
 
 startGame();
---
+++ script2.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+
+  //Create the variables
+  let SCORE;
+  
+
+  //Computer choice function
+  function computerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
+  }
+
+
+
+
+
+}
